ddb8d1a217cbafe299f1e09624b8a95ed8fe037c
Andrey Shitov <sai@arenadata.io>
2024-11-29 21:56:16 +0700

    37626-backport

--- tests/providers/hashicorp/_internal_client/test_vault_client.py
+++ tests/providers/hashicorp/_internal_client/test_vault_client.py
@@ -996,7 +996,7 @@ class TestVaultClient:
         )
         vault_client.create_or_update_secret(secret_path="path", secret={"key": "value"})
         mock_client.secrets.kv.v2.create_or_update_secret.assert_called_once_with(
-            mount_point="secret", secret_path="path", secret={"key": "value"}, cas=None
+            mount_point="secret", path="path", secret={"key": "value"}, cas=None
         )
 
     @mock.patch("airflow.providers.hashicorp._internal_client.vault_client.hvac")
@@ -1028,7 +1028,7 @@ class TestVaultClient:
         )
         vault_client.create_or_update_secret(secret_path="path", secret={"key": "value"}, cas=10)
         mock_client.secrets.kv.v2.create_or_update_secret.assert_called_once_with(
-            mount_point="secret", secret_path="path", secret={"key": "value"}, cas=10
+            mount_point="secret", path="path", secret={"key": "value"}, cas=10
         )
 
     @mock.patch("airflow.providers.hashicorp._internal_client.vault_client.hvac")
@@ -1046,7 +1046,7 @@ class TestVaultClient:
         )
         vault_client.create_or_update_secret(secret_path="path", secret={"key": "value"})
         mock_client.secrets.kv.v1.create_or_update_secret.assert_called_once_with(
-            mount_point="secret", secret_path="path", secret={"key": "value"}, method=None
+            mount_point="secret", path="path", secret={"key": "value"}, method=None
         )
 
     @mock.patch("airflow.providers.hashicorp._internal_client.vault_client.hvac")
@@ -1080,7 +1080,7 @@ class TestVaultClient:
         )
         vault_client.create_or_update_secret(secret_path="path", secret={"key": "value"}, method="post")
         mock_client.secrets.kv.v1.create_or_update_secret.assert_called_once_with(
-            mount_point="secret", secret_path="path", secret={"key": "value"}, method="post"
+            mount_point="secret", path="path", secret={"key": "value"}, method="post"
         )
 
     @mock.patch("airflow.providers.hashicorp._internal_client.vault_client.hvac")
