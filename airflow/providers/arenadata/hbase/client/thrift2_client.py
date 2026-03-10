08955b2c6dc61bd042dfa6f90f238f1b08cd73ef
dimitrionian <dmitry.bezlutzky@yandex.ru>
2026-03-10 19:59:36 +0500

    Remove redundant check

--- airflow/providers/arenadata/hbase/client/thrift2_client.py
+++ airflow/providers/arenadata/hbase/client/thrift2_client.py
@@ -284,8 +284,7 @@ class HBaseThrift2Client:
                 self._transport.open()
 
                 # Test connection
-                if self._client:
-                    self._client.getTableNamesByPattern(regex=None, includeSysTables=False)
+                self._client.getTableNamesByPattern(regex=None, includeSysTables=False)
 
                 logger.info(
                     "Successfully connected to HBase Thrift2 at %s:%s (SSL: %s, Transport: %s)",
